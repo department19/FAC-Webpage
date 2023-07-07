0e43ee16a5d01f69273af527311eac7ace9a4633
Jack Lam <jackktlam@gmail.com>
2023-07-07 21:46:42 +0100

    added basic elements for navbar

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     // for the side bar
     const sec = document.getElementById("sec");
-
+    const navbar = document.getElementById("navbar");
+    const navbutton = document.getElementById("navbutton");
     // function to toggle side bar
     function toggle() {
         sec.classList.toggle("active"); 
+        navbar.classList.toggle("active"); 
     }
 
     // tracks the side bar toggle
-    sec.addEventListener("click", toggle);
+    navbutton.addEventListener("click", toggle);
 });
